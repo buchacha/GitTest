44d03d85b44476ffcde193d6f0b7bfa63bcc8610
a <1@1.com>
2020-03-11 13:34:17 +0300

    second commit

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t = findViewById(R.id.text);
+        t.setText("Buchacha was here");
     }
 }
